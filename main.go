09062b36fc484ba84ae48f2538afab20cb5a2d00
Stelian <stoianstelian33@gmail.com>
2021-07-14 00:49:31 +0300

    removed MockClient test from main

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"store/pair"
 	"store/repository"
 	"store/server"
@@ -12,9 +11,5 @@ func main() {
 	repo, _ := repository.NewBoltRepository("db.db")
 	srv := service.NewService(repo, pair.StringPairFactory{})
 
-	go server.RunServer(srv, "9999")
-	go server.MockTestClient("9999")
-
-	var input string
-	_, _ = fmt.Scanln(&input)
+	server.RunServer(srv, "3000")
 }
